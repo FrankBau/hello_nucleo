c8ca1d7726f9edd3e0846cdf38484dda3db51728
Frank Bauernoeppel <frank@bauernoeppel.de>
2025-11-15 12:37:45 +0100

    gpio.h simplified

--- Inc/gpio.h
+++ Inc/gpio.h
@@ -8,13 +8,7 @@
 // pin mode
 #define INPUT 0
 #define OUTPUT 1
-#define ALT 2
-#define ANALOG 3
-// pull-up / pull-down
 #define INPUT_PULLUP 4
-#define INPUT_PULLDOWN 5
-// output type
-#define OUTPUT_OPEN_DRAIN 8
 
 // enable clock for a given GPIO port
 static inline void _enable_port_clock(GPIO_TypeDef *GPIOx) {
@@ -32,22 +26,16 @@ static inline void _enable_port_clock(GPIO_TypeDef *GPIOx) {
 static inline void gpio_setup(GPIO_TypeDef *GPIOx, int pin, uint8_t setup) {
     _enable_port_clock(GPIOx);
 
-    // MODER: 2 bits per pin
+    // MODER: 2 bits per pin: input, output, alternate function, analog
     int shift2 = pin * 2;
     uint32_t mode = setup & 0x3;
     GPIOx->MODER &= ~(0x3u << shift2);   // clear mode bits
     GPIOx->MODER |=  (mode << shift2);   // set new mode
 
-    // PUPDR: 2 bits per pin
+    // PUPDR: 2 bits per pin: no_pull, pull_up, pull_down
     uint32_t pull = (setup >> 2) & 0x3;
     GPIOx->PUPDR &= ~(0x3u << shift2);   // clear pull bits
     GPIOx->PUPDR |=  (pull << shift2);   // set new pull
-
-    // OTYPER: 1 bit per pin
-    int shift1 = pin;
-    uint32_t type = (setup >> 4) & 0x1;
-    GPIOx->OTYPER &= ~(0x1u << shift1);  // clear type bit
-    GPIOx->OTYPER |=  (type << shift1);  // set new type
 }
 
 // read GPIO pin state
